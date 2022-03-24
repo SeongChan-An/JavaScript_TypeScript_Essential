f46010d5651ae2754ceb9812dc4e49c3bd510714
Anphaca <69232581+SeongChan-An@users.noreply.github.com>
2022-03-24 22:45:07 +0900

    - Summary TS basic concept [Description] - Alias, Union.. [Relevant] -

---
+++ 04..BasicOfTypeScript/04..Alias.ts
@@ -0,0 +1,26 @@
+/**
+ * Type Aliases
+ */
+type Text = string;
+const name: Text = "seongchan";
+const address: Text = "korea";
+type Num = number;
+type Student = {
+  name: string;
+  age: number;
+};
+const student: Student = {
+  name: "seongchan",
+  age: 12,
+};
+
+/**
+ * String Literal Types
+ */
+type Name = "name";
+let ellieName: Name;
+ellieName = "name";
+type JSON = "json"; // 이미 JSON이 lib에 내장되어 interface로 선언되어 있는지 확인하기,
+const json: JSON = "json";
+
+type Boal = true;
