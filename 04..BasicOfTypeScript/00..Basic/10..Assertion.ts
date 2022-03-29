dc9eb761e390de930fa119ceb68066707074e852
Anphaca <69232581+SeongChan-An@users.noreply.github.com>
2022-03-29 21:26:39 +0900

    -Practice TypeScript [Description] - review the typescript basic [Relevant] -

--- 04..BasicOfTypeScript/00..Basic/10..Assertion.ts
+++ 04..BasicOfTypeScript/00..Basic/10..Assertion.ts
@@ -1,5 +1,7 @@
 /**
  * Type Assertions 💩
+ * 피하는 것이 좋기 때문에 사용할 때 충분히 고민 후에 사용하는 것이 좋다.
+ *
  */
 function jsStrFunc(): any {
   return 2;
@@ -9,12 +11,14 @@ console.log((result as string).length);
 console.log((<string>result).length);
 
 const wrong: any = 5;
-console.log((wrong as Array<number>).push(1)); // 😱
+console.log((wrong as Array<number>).push(1)); // 😱 error
 
 function findNumbers(): number[] | undefined {
   return undefined;
 }
 const numbers = findNumbers()!;
 numbers.push(2); // 😱
+// numbers!.push(2);
 
+// ! : ? 과는 반대로 무조건 값이 있는 경우, 잘 사용하지 않는다
 const button = document.querySelector("class")!;
---
+++ 04..BasicOfTypeScript/01..Practice/loading.ts
@@ -0,0 +1,43 @@
+{
+  /**
+   * Print Loading State
+   */
+
+  type LoadingState = {
+    state: 'loading';
+  };
+
+  type SuccessState = {
+    state: 'success';
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: 'fail';
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  printLoginState({ state: 'loading' }); // 👀 loading...
+  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case 'loading':
+        console.log('loading...');
+        break;
+      case 'success':
+        console.log(` Sucess ${state.response.body}`);
+        break;
+      case 'fail':
+        console.log(` Fail ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
+}
